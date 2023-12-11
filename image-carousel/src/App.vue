e604637109dcc9849fae0f94485208932b7b42f0
Tnixc <tnixxc@gmail.com>
2023-12-10 20:16:05 -0500

    Update image carousel styling

--- image-carousel/src/App.vue
+++ image-carousel/src/App.vue
@@ -10,23 +10,54 @@ export default {
 <template>
   <body class="h-screen flex items-center justify-center bg-neutral-100 p-20">
     <Flicking
-      class="ring-red-300 ring-4 h-full"
+      class="rounded-3xl h-full shadow-inner bg-gradient-to-br from-neutral-200 to-neutral-300 ring-4 ring-zinc-50"
       id="Flicking"
       :options="{ circular: true, moveType: 'snap' }"
       :viewportTag="'div'"
       :cameraTag="'div'">
-    <div class="h-full flex items-center justify-center">
-      <img class="max-w-[90%] max-h-[90%] rounded-2xl shadow-2xl" src="./assets/1.png" alt="" draggable="false">
-    </div>
-    <div class="h-full flex items-center justify-center">
-      <img class="max-w-[90%] max-h-[90%] rounded-2xl shadow-2xl" src="./assets/2.png" alt="" draggable="false">
-    </div>
-    <div class="h-full flex items-center justify-center">
-      <img class="max-w-[90%] max-h-[90%] rounded-2xl shadow-2xl" src="./assets/3.png" alt="" draggable="false">
-    </div>
+      <div class="h-full flex items-center justify-center">
+        <img
+          class="max-w-[90%] max-h-[90%] rounded-2xl shadow-2xl"
+          src="./assets/1.png"
+          alt=""
+          draggable="false" />
+      </div>
+      <div class="h-full flex items-center justify-center">
+        <img
+          class="max-w-[90%] max-h-[90%] rounded-2xl shadow-2xl"
+          src="./assets/2.png"
+          alt=""
+          draggable="false" />
+      </div>
+      <div class="h-full flex items-center justify-center">
+        <img
+          class="max-w-[90%] max-h-[90%] rounded-2xl shadow-2xl"
+          src="./assets/3.png"
+          alt=""
+          draggable="false" />
+      </div>
+      <div class="h-full flex items-center justify-center">
+        <img
+          class="max-w-[90%] max-h-[90%] rounded-2xl shadow-2xl"
+          src="./assets/4.png"
+          alt=""
+          draggable="false" />
+      </div>
+      <div class="h-full flex items-center justify-center">
+        <img
+          class="max-w-[90%] max-h-[90%] rounded-2xl shadow-2xl"
+          src="./assets/5.png"
+          alt=""
+          draggable="false" />
+      </div>
+      <div class="h-full flex items-center justify-center">
+        <img
+          class="max-w-[90%] max-h-[90%] rounded-2xl shadow-2xl"
+          src="./assets/6.png"
+          alt=""
+          draggable="false" />
+      </div>
     </Flicking>
   </body>
 </template>
-<style>
-
-</style>
+<style></style>
