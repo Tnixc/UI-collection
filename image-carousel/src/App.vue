152d5f6dfd94630ce3eb90680ec73c20403c83b6
Tnixc <tnixxc@gmail.com>
2023-12-10 20:14:23 -0500

    Update image carousel styling

--- image-carousel/src/App.vue
+++ image-carousel/src/App.vue
@@ -8,24 +8,25 @@ export default {
 };
 </script>
 <template>
-  <body class="h-screen flex items-center justify-center bg-neutral-900 p-20">
+  <body class="h-screen flex items-center justify-center bg-neutral-100 p-20">
     <Flicking
       class="ring-red-300 ring-4 h-full"
       id="Flicking"
       :options="{ circular: true, moveType: 'snap' }"
       :viewportTag="'div'"
       :cameraTag="'div'">
-    <div class="h-full bg-gradient-to-r from-teal-400 to-white flex items-center">
-      <img class="scale-90 max-h-[95%]" src="./assets/1.png" alt="" draggable="false">
+    <div class="h-full flex items-center justify-center">
+      <img class="max-w-[90%] max-h-[90%] rounded-2xl shadow-2xl" src="./assets/1.png" alt="" draggable="false">
     </div>
-    <div class="h-full bg-gradient-to-r from-teal-400 to-white flex items-center">
-      <img class="scale-90 max-h-[95%]" src="./assets/2.png" alt="" draggable="false">
+    <div class="h-full flex items-center justify-center">
+      <img class="max-w-[90%] max-h-[90%] rounded-2xl shadow-2xl" src="./assets/2.png" alt="" draggable="false">
     </div>
-    <div class="h-full bg-gradient-to-r from-teal-400 to-white flex items-center">
-      <img class="scale-90 max-h-[95%]" src="./assets/3.png" alt="" draggable="false">
+    <div class="h-full flex items-center justify-center">
+      <img class="max-w-[90%] max-h-[90%] rounded-2xl shadow-2xl" src="./assets/3.png" alt="" draggable="false">
     </div>
     </Flicking>
   </body>
 </template>
 <style>
+
 </style>
