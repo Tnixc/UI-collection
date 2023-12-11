b3d6186188f7eb61957d796c53362e149d712398
Tnixc <tnixxc@gmail.com>
2023-12-10 20:10:01 -0500

    Refactor image carousel layout

--- image-carousel/src/App.vue
+++ image-carousel/src/App.vue
@@ -15,27 +15,17 @@ export default {
       :options="{ circular: true, moveType: 'snap' }"
       :viewportTag="'div'"
       :cameraTag="'div'">
-      <div class=" bg-slate-800">
-        <img src="./assets/1.png" alt="1" draggable="false" />
-      </div>
-      <div class=" bg-slate-800">
-        <img src="./assets/2.png" alt="2" draggable="false" />
-      </div>
-      <div class=" bg-slate-800">
-        <img src="./assets/3.png" alt="3" draggable="false" />
-      </div>
-      <div class=" bg-slate-800">
-        <img src="./assets/4.png" alt="4" draggable="false" />
-      </div>
-      <div class=" bg-slate-800">
-        <img src="./assets/5.png" alt="5" draggable="false" />
-      </div>
-      <div class=" bg-slate-800">
-        <img src="./assets/6.png" alt="6" draggable="false" />
-      </div>
+    <div class="h-full bg-gradient-to-r from-teal-400 to-white flex items-center">
+      <img class="scale-90 max-h-[95%]" src="./assets/1.png" alt="" draggable="false">
+    </div>
+    <div class="h-full bg-gradient-to-r from-teal-400 to-white flex items-center">
+      <img class="scale-90 max-h-[95%]" src="./assets/2.png" alt="" draggable="false">
+    </div>
+    <div class="h-full bg-gradient-to-r from-teal-400 to-white flex items-center">
+      <img class="scale-90 max-h-[95%]" src="./assets/3.png" alt="" draggable="false">
+    </div>
     </Flicking>
   </body>
 </template>
 <style>
-
 </style>
