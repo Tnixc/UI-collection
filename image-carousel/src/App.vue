c0872cc08d5cb24b446934706a48f08f5b0f9a95
Tnixc <tnixxc@gmail.com>
2023-12-10 19:54:14 -0500

    Update image carousel component in App.vue

--- image-carousel/src/App.vue
+++ image-carousel/src/App.vue
@@ -3,15 +3,39 @@ import Flicking from "@egjs/vue3-flicking";
 import "@egjs/flicking/dist/flicking.css";
 export default {
   components: {
-    Flicking
-  }
-}
+    Flicking,
+  },
+};
 </script>
 <template>
-  <Flicking class=" m-20 w-40 h-40">
-  <!-- <Flicking :options="{ circular: true }"> -->
-    <div class="panel h-40 w-40 bg-red-400">Panel 1</div>
-    <div class="panel h-40 w-40 bg-blue-400">Panel 2</div>
-    <div class="panel h-40 w-40 bg-emerald-400">Panel 3</div>
-  </Flicking>
+  <body class="h-screen flex items-center justify-center bg-neutral-900 p-20">
+    <Flicking
+      class="ring-red-300 ring-4 h-full"
+      id="Flicking"
+      :options="{ circular: true, moveType: 'snap' }"
+      :viewportTag="'div'"
+      :cameraTag="'div'">
+      <div class=" bg-slate-800">
+        <img src="./assets/1.png" alt="1" draggable="false" />
+      </div>
+      <div class=" bg-slate-800">
+        <img src="./assets/2.png" alt="2" draggable="false" />
+      </div>
+      <div class=" bg-slate-800">
+        <img src="./assets/3.png" alt="3" draggable="false" />
+      </div>
+      <div class=" bg-slate-800">
+        <img src="./assets/4.png" alt="4" draggable="false" />
+      </div>
+      <div class=" bg-slate-800">
+        <img src="./assets/5.png" alt="5" draggable="false" />
+      </div>
+      <div class=" bg-slate-800">
+        <img src="./assets/6.png" alt="6" draggable="false" />
+      </div>
+    </Flicking>
+  </body>
 </template>
+<style>
+
+</style>
