469f411c8477a4c44f1c521d2d2cb9b480fb65b6
Tnixc <tnixxc@gmail.com>
2023-12-10 19:36:12 -0500

    Add @egjs/flicking-plugins dependency and implement image carousel

--- image-carousel/src/App.vue
+++ image-carousel/src/App.vue
@@ -1,8 +1,17 @@
 <script lang="ts">
+import Flicking from "@egjs/vue3-flicking";
+import "@egjs/flicking/dist/flicking.css";
+export default {
+  components: {
+    Flicking
+  }
+}
 </script>
 <template>
-  
+  <Flicking class=" m-20 w-40 h-40">
+  <!-- <Flicking :options="{ circular: true }"> -->
+    <div class="panel h-40 w-40 bg-red-400">Panel 1</div>
+    <div class="panel h-40 w-40 bg-blue-400">Panel 2</div>
+    <div class="panel h-40 w-40 bg-emerald-400">Panel 3</div>
+  </Flicking>
 </template>
-<style scoped>
-
-</style>
